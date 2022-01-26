823e69be379aa606e7cddc759cff7470a44b3c58
Mehrnaz <mehrnazhemmat1998@gmail.com>
2022-01-26 23:57:22 +0330

    some changes added

--- Question 1/script-1.js
+++ Question 1/script-1.js
@@ -1,12 +1,19 @@
 function myFunction(e) {
-   e.preventDefault();
-   const forElement = e.target;
-   const values = {};
-    for (input of forElement) {
-        if(input.name) {
-            values[input.name]
-        }
-        console.log(values)
-    }
+ const firstNameInput = document.getElementById("firstnameinput").value;
+ const lastNameInput = document.getElementById("lastnameinput").value;
+ const selectInput = document.getElementById("country").value;
+const description = document.getElementById("description").value;
+
+alert("You're information has succesfully registered!")
+const myPreventEvent = false;
+const mySetTimeOut = setTimeout(() => {
+  if(myPreventEvent == flase) {
+    e.preventDefault()
+    
+  }else {
+
+  }  
+}, 8000);
 
   }
+  
