59e4c6efa41c182910b1c0d69cb5671908e636d3
Mehrnaz <mehrnazhemmat1998@gmail.com>
2022-01-28 02:33:09 +0330

    add modal to q-1

--- Question 1/script-1.js
+++ Question 1/script-1.js
@@ -1,19 +1,40 @@
 function myFunction(e) {
- const firstNameInput = document.getElementById("firstnameinput").value;
- const lastNameInput = document.getElementById("lastnameinput").value;
- const selectInput = document.getElementById("country").value;
-const description = document.getElementById("description").value;
+  const firstNameInput = document.getElementById("firstnameinput").value;
+  const lastNameInput = document.getElementById("lastnameinput").value;
+  const selectInput = document.getElementById("country").value;
+  const description = document.getElementById("description").value;
 
-alert("You're information has succesfully registered!")
-const myPreventEvent = false;
-const mySetTimeOut = setTimeout(() => {
-  if(myPreventEvent == flase) {
-    e.preventDefault()
-    
-  }else {
 
-  }  
-}, 8000);
-
-  }
   
+
+
+const reloadPage = setTimeout(() => {
+  e.location.reload;
+},8000);
+console.log("this is after setTimeout")
+}
+
+
+// ===============================================
+var modal = document.getElementById("myModal");
+
+var btn = document.getElementById("myBtn");
+
+var span = document.getElementById("close");
+
+// When the user clicks the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
