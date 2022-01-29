cb13c19db8e92453d4f8b99e93bdb5b9df279a21
Mehrnaz <mehrnazhemmat1998@gmail.com>
2022-01-29 23:47:56 +0330

    some changes on q-1

--- Question 1/script-1.js
+++ Question 1/script-1.js
@@ -1,40 +1,46 @@
-function myFunction(e) {
-  const firstNameInput = document.getElementById("firstnameinput").value;
-  const lastNameInput = document.getElementById("lastnameinput").value;
-  const selectInput = document.getElementById("country").value;
-  const description = document.getElementById("description").value;
+document.getElementById('myBtn').addEventListener('click', function (event) {
+  event.preventDefault()
+})
 
-
-  
-
-
-const reloadPage = setTimeout(() => {
-  e.location.reload;
-},8000);
-console.log("this is after setTimeout")
+userInfo = []
+function myFunction() {
+  return new Promise((resolve, reject) => {
+    const firstNameInput = document.getElementById('firstnameinput').value
+    const lastNameInput = document.getElementById('lastnameinput').value
+    const selectInput = document.getElementById('country').value
+    const description = document.getElementById('description').value
+    alert('your information was sucessfully registered!')
+    userInfo.push({
+      Name: firstNameInput,
+      Lastname: lastNameInput,
+      Nationality: selectInput,
+      description: description,
+    })
+    setTimeout(() => {
+      document.location.reload()
+    }, 8000)
+  })
 }
 
 
 // ===============================================
-var modal = document.getElementById("myModal");
-
-var btn = document.getElementById("myBtn");
-
-var span = document.getElementById("close");
+var modal = document.getElementById('myModal')
+var btn = document.getElementById('myBtn')
+var span = document.getElementById('close')
 
 // When the user clicks the button, open the modal
 btn.onclick = function () {
-  modal.style.display = "block";
-};
+  modal.style.display = 'block'
+}
 
-// When the user clicks on <span> (x), close the modal
+//When the user clicks on <span> (x), close the modal
 span.onclick = function () {
-  modal.style.display = "none";
-};
+  modal.style.display = 'none'
+}
 
-// When the user clicks anywhere outside of the modal, close it
+//When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
   if (event.target == modal) {
-    modal.style.display = "none";
+    modal.style.display = 'none'
   }
-};
+}
