19308ba18c0da9a76d532a92c0be8600725cab5e
Mehrnaz <mehrnazhemmat1998@gmail.com>
2022-01-30 14:21:06 +0330

    some changes on q-1

--- Question 1/script-1.js
+++ Question 1/script-1.js
@@ -1,46 +1,48 @@
-document.getElementById('myBtn').addEventListener('click', function (event) {
-  event.preventDefault()
-})
+document.getElementById("myBtn").addEventListener("click", function (event) {
+  event.preventDefault();
+});
 
-userInfo = []
+userInfo = [];
 function myFunction() {
   return new Promise((resolve, reject) => {
-    const firstNameInput = document.getElementById('firstnameinput').value
-    const lastNameInput = document.getElementById('lastnameinput').value
-    const selectInput = document.getElementById('country').value
-    const description = document.getElementById('description').value
-    alert('your information was sucessfully registered!')
+    const firstNameInput = document.getElementById("firstnameinput").value;
+    const lastNameInput = document.getElementById("lastnameinput").value;
+    const selectInput = document.getElementById("country").value;
+    const description = document.getElementById("description").value;
+
     userInfo.push({
       Name: firstNameInput,
       Lastname: lastNameInput,
       Nationality: selectInput,
       description: description,
-    })
+    });
+    console.log(userInfo);
     setTimeout(() => {
-      document.location.reload()
-    }, 8000)
-  })
+      document.location.reload();
+      // location.replace("https://www.w3schools.com");
+    }, 8000);
+  });
 }
 
-
 // ===============================================
-var modal = document.getElementById('myModal')
-var btn = document.getElementById('myBtn')
-var span = document.getElementById('close')
+
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementById("close");
 
 // When the user clicks the button, open the modal
 btn.onclick = function () {
-  modal.style.display = 'block'
-}
+  modal.style.display = "block";
+};
 
 //When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = 'none'
-}
+// span.onclick = function () {
+//   modal.style.display = "none";
+// };
 
 //When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
   if (event.target == modal) {
-    modal.style.display = 'none'
+    modal.style.display = "none";
   }
-}
+};
